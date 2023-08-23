c4431104b37a80677922e8e7c30fe9bd889c32fb
vin2410 <lethanhvinh2410@gmail.com>
2023-08-23 22:25:22 +0700

    Recheck again to find mistake but not work

--- Form/index.js
+++ Form/index.js
@@ -9,39 +9,21 @@ form.addEventListener("submit", (e) => {
     validateInputs();
 });
 
-const setError = (element, message) => {
-    const formInfo = element.parentElement;
-    const errorMessage = formInfo.querySelector("p");
-    console.log(formInfo);
-    errorMessage.innerText = message;
-    formInfo.className = "form-info error";
-};
-
-// Right form submit
-const setSuccess = (element) => {
-    const formInfo = element.parentElement;
-    formInfo.className = "form-info success";
-};
-
-// Validation Email
-const isEmail = (email) => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-
-
-// Validation phone number
-
-const isPhone = (phone) => /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/.test(phone);
-
+// Main function
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const phoneValue = phone.value.trim();
     const contentValue = content.value.trim();
 
+    console.log(emailValue);
+
     if (usernameValue === "") {
         setError(username, "Please enter your username");
     } else {
         setSuccess(username);
     }
+
     if (emailValue === "") {
         setError(email, "Please enter your email");
     } else if (!isEmail(email)) {
@@ -64,3 +46,26 @@ const validateInputs = () => {
         setSuccess(content);
     }
 };
+
+// Input error
+const setError = (element, message) => {
+    const formInfo = element.parentElement;
+    const errorMessage = formInfo.querySelector("p");
+    console.log(formInfo);
+    errorMessage.innerText = message;
+    formInfo.className = "form-info error";
+};
+
+// Input success
+const setSuccess = (element) => {
+    const formInfo = element.parentElement;
+    console.log(formInfo);
+    formInfo.className = "form-info success";
+};
+
+// Validation Email
+const isEmail = (email) =>
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+// Validation phone number
+const isPhone = (phone) => /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/.test(phone);
