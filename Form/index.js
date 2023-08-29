d6b04e5fb746abb9b121d177f8e489556ee9b766
vin2410 <lethanhvinh2410@gmail.com>
2023-08-29 10:59:03 +0700

    Change mindset to use object formField

--- Form/index.js
+++ Form/index.js
@@ -1,67 +1,64 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const content = document.getElementById("description");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    validateForms();
-});
-
-const validateForms = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const phoneValue = phone.value.trim();
-    const contentValue = content.value.trim();
-
-    if (usernameValue === "") {
-        setError(username, "Please enter your username");
-    } else {
-        setSuccess(username);
-    }
-
-    if (emailValue === "") {
-        setError(email, "Please enter your email");
-    } else if (!isEmail(emailValue)) {
-        setError(email, "Please enter a valid email");
-    } else {
-        setSuccess(email);
-    }
-
-    if (phoneValue === "") {
-        setError(phone, "Please enter your phone number");
-    } else if (!isPhone(phoneValue)) {
-        setError(phone, "Please enter a valid phone number");
-    } else {
-        setSuccess(phone);
-    }
-
-    if (contentValue === "") {
-        setError(content, "Please enter your content");
-    } else {
-        setSuccess(content);
-    }
-};
-// Input error
-const setError = (element, message) => {
-    const formInfo = element.parentElement;
-    const errorMessage = formInfo.querySelector("p");
-    errorMessage.innerText = message;
-    formInfo.className = "form-info error";
+// New way with object
+const formField = {
+    username: {
+        key: "username",
+        requiredMessage: "Please enter a username",
+    },
+    email: {
+        key: "email",
+        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        requiredMessage: "Please enter an email",
+        invalidMessage: "Please enter an valid email",
+    },
+    phone: {
+        key: "phone",
+        regex: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/,
+        requiredMessage: "Please enter a number phone",
+        invalidMessage: "Please enter an valid number",
+    },
+    description: {
+        key: "description",
+        requiredMessage: "Please enter a description about yourself",
+    },
 };
 
-// Input success
-const setSuccess = (element) => {
-    const formInfo = element.parentElement;
-    formInfo.className = "form-info success";
+const validateForm = (event) => {
+    event.preventDefault();
+    const errors = {};
+    const form = document.getElementById("form");
+
+    for (let field in formField) {
+        const inputElement = document.getElementById(field);
+        const fieldValue = inputElement.value.trim();
+        const fieldConfig = formField[field];
+
+        if (fieldConfig.requiredMessage && fieldValue === "") {
+            errors[field] = fieldConfig.requiredMessage;
+        } else if (fieldConfig.regex && !fieldConfig.regex.test(fieldValue)) {
+            errors[field] = fieldConfig.invalidMessage;
+        } else {
+            const inputElement = document.getElementById(field);
+            const formInfo = inputElement.parentElement;
+            const errorElement = formInfo.querySelector("p");
+            formInfo.className = "form-info success";
+            errorElement.textContent = "";
+        }
+    }
+    console.log(errors);
+
+    if (Object.keys(errors).length > 0) {
+        displayErrors(errors);
+    } else {
+        form.submit();
+    }
 };
 
-// Validation Email
-const isEmail = (email) =>
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-    );
-
-// Validation phone number
-const isPhone = (phone) => /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/.test(phone);
+const displayErrors = (errors) => {
+    for (let field in errors) {
+        const inputElement = document.getElementById(field);
+        const formInfo = inputElement.parentElement;
+        const errorElement = formInfo.querySelector("p");
+        formInfo.className = "form-info error";
+        errorElement.textContent = errors[field];
+    }
+};
