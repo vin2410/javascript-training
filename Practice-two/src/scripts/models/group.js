11dfe9f8dede9c22a00d633311baef08d953adbe
vin2410 <lethanhvinh2410@gmail.com>
2023-10-03 10:47:29 +0700

    Split model

---
+++ Practice-two/src/scripts/models/group.js
@@ -0,0 +1,18 @@
+import { getGroup, postItem, patchItem, deleteItem } from '../services/apiService';
+
+export default class Group {
+    constructor() {
+        this.groups = [];
+    }
+    getGroupModel = () => {
+        return getGroup().then((response) => {
+            this.groups = response;
+            return response;
+        });
+    };
+    addGroupModel = (data) => {
+        const response = postItem(data);
+        this.groups.push(response);
+        return response;
+    };
+}
