13d4d1873b1d3274bd43245b3d98c546a0ccda9d
vin2410 <lethanhvinh2410@gmail.com>
2023-10-07 13:24:20 +0700

    Split controller

--- Practice-two/src/scripts/views/view.js
+++ Practice-two/src/scripts/views/view.js
@@ -1,35 +1,7 @@
-import Template from '../templates/templates';
+import GroupView from './groupView';
+
 export default class View {
     constructor() {
-        this.groupListEl = document.querySelector('.group');
-        this.addGroupBtn = document.querySelector('.icon-folder-plus');
+        this.group = new GroupView();
     }
-
-    renderGroupList = (groups) => {
-        groups.forEach((group) => {
-            const groupTemplate = Template.renderGroup(group);
-            this.groupListEl.innerHTML += groupTemplate;
-        });
-    };
-
-    addGroupView = (addGroup) => {
-        this.addGroupBtn.addEventListener('click', (e) => {
-            e.preventDefault;
-            alert('click success');
-            const groupValue = {
-                title: '',
-                lists: [
-                    {
-                        name: '',
-                        tasks: [],
-                    },
-                ],
-            };
-            addGroup(groupValue);
-        });
-    };
-    displayGroupList = (group) => {
-        const groupTemplate = Template.renderGroup(group);
-        this.groupListEl.innerHTML += groupTemplate;
-    };
 }
