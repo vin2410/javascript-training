0809488349269fb82085bfcdec3317ef927d3760
vin2410 <lethanhvinh2410@gmail.com>
2023-10-17 09:49:01 +0700

    fix template

--- Practice-two/src/scripts/templates/group.js
+++ Practice-two/src/scripts/templates/group.js
@@ -4,15 +4,6 @@ export default class GroupTemplate {
     renderGroup = (group) =>
         `
         <li>
-<<<<<<< HEAD
-            <div class="group-item__title">
-                <span class="icon icon-list"></span>
-                <input type="text" readonly value=${group.title}/>
-            </div>
-            <div class="group-item__list">
-                <span class="icon icon-menu"></span>
-                <input type="text" readonly value=${group.lists.name} />
-=======
             <div class="group-title">
                 <span class="icon icon-list"></span>
                 <input class="group-title__input" type="text" readonly value=${group.title} data-id = ${group.id}/>
@@ -20,7 +11,6 @@ export default class GroupTemplate {
             <div class="group-list">
                 <span class="icon icon-menu"></span>
                 <input type="text" readonly value=${group.lists[0].name} />
->>>>>>> demo
             </div>
         </li>
     `;
